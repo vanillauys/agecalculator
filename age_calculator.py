30ecb0c1326811580312493852770738370c1726
Wihan Uys <wihan.uys@gmail.com>
2022-02-03 16:59:01 +0200

    Initial commit

---
+++ age_calculator.py
@@ -0,0 +1,3 @@
+def get_age(start_date, end_date):
+    """Returns the age (start date to end date)
+    in years, months, weeks, days, hours, minutes and seconds"""
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import age_calculator
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def get_age():
+    if request.method == 'POST':
+        start_date = request.form["start_date"]
+        end_date = request.form["end_date"]
+        age = age_calculator.get_age(start_date, end_date)
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
